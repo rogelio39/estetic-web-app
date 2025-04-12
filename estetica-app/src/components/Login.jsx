607b0706b546dab37eeb90ce816bcc660c35ae73
Rogelio Suleta <andresrogesu@gmail.com>
2025-04-12 15:51:36 -0300

    ..

--- estetica-app/src/components/Login.jsx
+++ estetica-app/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { auth, googleProvider, signInWithPopup } from '../firebase';
+import { auth, googleProvider } from '../firebase';
+import { signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-
 
 
 const Login = () => {
